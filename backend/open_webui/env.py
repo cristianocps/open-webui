d55d538e93ba0ed6eec5fb8d9c7872120ef851f7
Cristiano Silva <cristiano@apdigitalservices.com.br>
2025-01-23 11:40:30 -0300

    Adicionado docker compose inicial

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -104,9 +104,9 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "ContentFly")
+if WEBUI_NAME != "ContentFly":
+    WEBUI_NAME += " (ContentFly)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
